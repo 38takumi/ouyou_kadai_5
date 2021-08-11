48cbd18b120c948391409e71e5d1a8d32fea5830
38takumi <saitout485@gmail.com>
2021-08-11 13:09:56 +0000

    エラー直し

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,12 +2,12 @@ class SearchesController < ApplicationController
   def search
     @range = params[:range]
     search = params[:search]
-  　word = params[:word]
+    @word = params[:word]
 
-  　if @range == '1'
-    @user = User.search(search,word)
+    if @range == '1'
+    @user = User.search(search,@word)
     else
-  　  @book = Book.search(search,word)
-  　end
+     @book = Book.search(search,@word)
+    end
   end
 end
